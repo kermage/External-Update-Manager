24802bc020f355efcd1f5d1f0515aa7072b45ccb
kermage <genealyson.torcende@gmail.com>
2017-09-12 22:12:25 +0800

    gett the current version

--- external-updater.php
+++ external-updater.php
@@ -7,11 +7,13 @@
 
 class External_Updater {
 
+	public $fullpath;
 	public $type;
 	public $slug;
 	public $transient;
 
 	public function __construct( $fullpath ) {
+		$this->fullpath = $fullpath;
 		$this->get_details( $fullpath );
 		$this->transient = 'external_updater_' . $this->type . '_' . $this->slug;
 
@@ -47,8 +49,26 @@ class External_Updater {
 		$status = new StdClass;
 		$status->last_checked = time();
 		$status->update = null;
+		$status->current_version = $this->get_current_version();
 
 		return $status;
+	}
+
+	public function get_current_version() {
+		if ( $this->type == 'theme' ) {
+			$data = wp_get_theme( $this->slug );
+			$version = $data->get( 'Version' );
+		} else {
+			if ( ! function_exists( 'get_plugin_data' ) ) {
+				require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+			}
+
+			$data = get_plugin_data( $this->fullpath, false, false );
+			$version = $data['Version'];
+		}
+
+		return $version;
+		}
 	}
 
 }
